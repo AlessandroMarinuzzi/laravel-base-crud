38286c529f9702334fdff8a500286c3c1ae4450d
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-07-14 17:27:28 +0100

    - Modify form fields.

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -4,34 +4,34 @@
 
 
 @section('main_content')
-<div class="container">
+<div class="add_comic_form container">
     <h1>Add a comic</h1>
     <form action="{{route('comics.store')}}" method="POST">
         @csrf
         <div class="form_group">
-            <label for="title">Cover Image</label>
-            <input type="text" name="title" id="title" placeholder="Inserisci il titolo" aria-describedby="helpId">
+            <label for="cover">Cover Image</label>
+            <input type="text" name="cover" id="cover" placeholder="Insert Image Link">
             <small id="helpId">Paste Image Link Address</small>
         </div>
         <div class="form_group">
-            <label for="description">Title</label>
-            <textarea name="description" id="description" rows="3"></textarea>
+            <label for="title">Title</label>
+            <input name="title" id="title" placeholder="Insert Title"></input>
             <small id="helpId">Insert Comic Title</small>
         </div>
         <div class="form_group">
-            <label for="thumb">Plot</label>
-            <input type="text" name="thumb" id="thumb" placeholder="url" aria-describedby="helpId">
+            <label for="plot">Plot</label>
+            <textarea name="plot" id="plot" rows="5"></textarea>
             <small id="helpId">Insert Comic Plot</small>
         </div>
         <div class="form_group">
-            <label for="price">Artist</label>
-            <input type="text" name="price" id="price" placeholder="prezzo" aria-describedby="prezzo">
-            <small id="prezzo">Insert Comic Artist</small>
+            <label for="artist">Artist</label>
+            <input type="text" name="artist" id="artist" placeholder="Insert Artist Full Name" aria-describedby="artist">
+            <small id="artist">Insert Comic Artist</small>
         </div>
         <div class="form_group">
             <label for="series">Price</label>
-            <input type="text" name="series" id="series" placeholder="serie" aria-describedby="prezzo">
-            <small id="prezz">Insert Comic Price</small>
+            <input type="text" name="price" id="price" placeholder="Insert Price" aria-describedby="price">
+            <small id="price">Insert Comic Price</small>
         </div>
         <button type="submit">Generate</button>
     </form>
