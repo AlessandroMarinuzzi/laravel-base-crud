2051ad673d3c8ef1352a3aa3c3024c8ad79798bb
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-07-14 16:22:12 +0100

    - Create link routing to comics page. - Display Comics and related details.

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+@section('title','It is Comic Home')
+
+
+@section('main_content')
+
+<h1>It is Comic Home</h1>
+<div class="container">
+    @foreach($comics as $comic)
+        <div class="comic_card">
+            <img width="150" src="{{$comic->cover}}" alt="">
+            <h2 class="title">{{$comic->title}}</h2>
+            <p class="plot">{{$comic->plot}}</p>
+            <h3 class="artist">{{$comic->artist}}</h3>
+            <span class="price">{{$comic->price}}</span>
+        </div>
+    @endforeach
+</div>
+@endsection
